0a1bbcd671e55bce7d6d056500721db74598c0fd
Kristian Wahlroos <kjhwah@utu.fi>
2019-02-26 16:26:34 +0200

    Create a branch for local server and create example implementation for gathering data from json.

--- __init__.py
+++ __init__.py
@@ -1,35 +1,47 @@
 import os
-from google.cloud import storage
+import time
+import json
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
-bucket_name = 'hurdling.appspot.com'
-UPLOAD_FOLDER = '/tmp'
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLD = '/Path'
+UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_FOLD)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = "TESTSTRING"
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    print(request)
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            name = '/{}/{}'.format(bucket_name, filename)
-            storage_client = storage.Client()
-            bucket = storage_client.get_bucket(bucket_name)
-            blob = storage.Blob(name, bucket)
-            blob.upload_from_filename(filepath)
-    return "Works"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if 'json' not in request.files:
+            flash('No json part')
+            return redirect(request.url)
+        json_data = request.files['json']
+        json_thing = json.load(json_data)
+        print("This is an example how to call a first corner location's y-coordinate: " + json_thing['cornerLocations'][0][1])
+    return "It returns this"
+from werkzeug import SharedDataMiddleware
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
