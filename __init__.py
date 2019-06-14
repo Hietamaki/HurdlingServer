d577d742bf939c47da86c9c319167a6cf5621b43
Santtu Hietamäki <samhie@utu.fi>
2019-06-14 14:48:49 +0300

    Fix compile errors and convert spaces to tabs

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLD = '/Path'
+UPLOAD_FOLD = 'videos'
 UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_FOLD)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4'])
 
@@ -14,34 +14,39 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = "TESTSTRING"
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    print(request)
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        if 'json' not in request.files:
-            flash('No json part')
-            return redirect(request.url)
-        json_data = request.files['json']
-        json_thing = json.load(json_data)
-        print("This is an example how to call a first corner location's y-coordinate: " + json_thing['cornerLocations'][0][1])
-    return "It returns this"
+	if request.method == 'POST':
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		if 'json' not in request.files:
+			flash('No json part')
+			return redirect(request.url)
+		json_data = request.files['json']
+		json_thing = json.load(json_data)
+		print("This is an example how to call a first corner location's y-coordinate: " + str(json_thing['cornerLocations'][0][1]))
+
+		# Start analysis
+
+	return "It returns this"
+
 from werkzeug import SharedDataMiddleware
 app.add_url_rule('/uploads/<filename>', 'uploaded_file',
-                 build_only=True)
+	build_only=True)
+
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-    '/uploads':  app.config['UPLOAD_FOLDER']
+	'/uploads':  app.config['UPLOAD_FOLDER']
 })
